48147e758ef5ad5dd7cd4ee02da1a61594dbe932
MAHENDRA REDDY <70009808+mahiprime2001@users.noreply.github.com>
2022-09-02 09:08:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,41 @@
+<?php require("login.class.php") ?>
+<?php 
+	include 'config.php';
 
+	if(isset($_POST['submit'])){
+		$user = new LoginUser($_POST['email'], $_POST['password']);
+		
+		session_start();
 
-<?php
+		if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    		header("location: user_profile.php");
+    		exit;
+		}
 
-include 'config.php';
-session_start();
+		$email = $password = "";
+		$email_err = $password_err = $login_err = "";
+   		$email = mysqli_real_escape_string($conn, $_POST['email']);
+   		$pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   		$select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
-if(isset($_POST['submit'])){
-
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-
-   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
-
-   if(mysqli_num_rows($select) > 0){
-      $row = mysqli_fetch_assoc($select);
-      $_SESSION['user_id'] = $row['id'];
-      header('location:user_profile.php');
-   }else{
-      $message[] = 'incorrect email or password!';
-   }
-
-}
-
+   		if (empty(trim($_POST["email"]))){
+			$email_err = "Please enter Email.";
+   		}
+		else if(empty(trim($_POST["password"]))){
+			$password_err = "Please enter your password.";
+		}
+   		else if(mysqli_num_rows($select) > 0){
+			$row = mysqli_fetch_assoc($select);
+			$_SESSION['user_id'] = $row['id'];
+			header('location:user_profile.php');
+ 		}
+   		else{
+      		$message[] = 'incorrect email or password!';
+   		}
+	}
 ?>
+
+
 
 <!doctype html>
 <html lang="en">
@@ -101,9 +114,7 @@ if(isset($_POST['submit'])){
   <script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/main.js"></script>
-  <script>
-
-  </script>
+  <script src="js/loginajax.js"></script>
 
 	</body>
 </html>
